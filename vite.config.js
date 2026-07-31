0f2a688b94bce4a32195757ebf4f554e4a2cdb51
npx-official <youssef.game97u3@gamil.com>
2026-07-31 22:14:59 +0100

    Update vite.config.js to include external writeups folder

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // هذا السطر هو الحل السحري لإخفاء شعار Vue DevTools
+  // هذا القسم سيجعل Vite يبحث عن الملفات في writeups و public
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url))
+      }
+    },
+    assetsInlineLimit: 0
+  },
   define: {
     '__VUE_PROD_DEVTOOLS__': false
   }
